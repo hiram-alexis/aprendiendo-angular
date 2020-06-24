59f8b87edad352941cd5fec76a91c374d7f21a09
Hiram Alexis Baños Sánchez <hiram.alexis1706@gmail.com>
2020-06-24 03:45:04 -0500

    Intento sin exito
    me sigue marcando un error en a direccion del componente. #1

---
+++ src/app/videojuego/videojuego.componet.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'videojuego',
+	template: `
+		<ul>
+			<li>GTA</li>
+			<li>Prince of Persia</li>
+			<li>Tekken</li>
+			<li>Mario</li>
+		</ul>
+	`
+	})
+
+export class VideojuegoComponent {
+	
+}
+/*
+import { Component} from '@angular/core';
+
+@Component({
+	selector: 'videojuego',
+	template: `
+			<h2>Componente de Videojuegos </h2>
+			<ul>
+			<li>GTA</li>
+			<li>Prince of Persia</li>
+			<li>Tekken</li>
+			<li>Mario</li>
+			</ul>
+	`
+	})
+export class VideojuegoComponent {
+	
+}
+*/
